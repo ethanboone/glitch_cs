4cc1f5cea5dc11c1385be2b1306f7a15f2523cfe
ethanboone <eboone2019@gmail.com>
2021-06-20 11:05:43 -0600

    added startup

--- glitchserver/Startup.cs
+++ glitchserver/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,8 +13,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySqlConnector;
 
-namespace glitchserver
+namespace glitchserver.Server
 {
     public class Startup
     {
@@ -26,12 +29,57 @@ namespace glitchserver
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureCors(services);
+            ConfigureAuth(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "glitchserver", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "keepr.Server", Version = "v1" });
             });
+
+            services.AddScoped<IDbConnection>(x => CreateDbConnection());
+
+        }
+
+        private void ConfigureCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsDevPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins(new string[]{
+                        "http://localhost:8080", "http://localhost:8081"
+                    });
+                });
+            });
+        }
+
+        private void ConfigureAuth(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                // NOTE this must match the object structure in appsettings.json
+                options.Authority = $"https://{Configuration["Domain"]}/";
+                options.Audience = Configuration["Audience"];
+            });
+
+        }
+
+
+        private IDbConnection CreateDbConnection()
+        {
+            // NOTE this must match the object structure in appsettings.json
+            string connectionString = Configuration["CONNECTION_STRING"];
+            return new MySqlConnection(connectionString);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,12 +89,19 @@ namespace glitchserver
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "glitchserver v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "keepr.Server v1"));
+                app.UseCors("CorsDevPolicy");
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+
+            // NOTE use to serve your built client
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
