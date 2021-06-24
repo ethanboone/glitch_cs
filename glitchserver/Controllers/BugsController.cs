02b4fafb34888db1b630a478987f4c4ee011a04e
ethanboone <eboone2019@gmail.com>
2021-06-23 18:50:55 -0600

    added get all/get one bugs

--- glitchserver/Controllers/BugsController.cs
+++ glitchserver/Controllers/BugsController.cs
@@ -1,7 +1,51 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using glitchserver.Services;
+using glitchserver.Models;
+using Microsoft.AspNetCore.Mvc;
+using CodeWorks.Auth0Provider;
+using Microsoft.AspNetCore.Authorization;
+
 namespace glitchserver.Controllers
 {
-    public class BugsController
+    [ApiController]
+    [Route("[controller]")]
+    public class BugsController : ControllerBase
     {
-        
+        private readonly BugsService _service;
+
+        public BugsController(BugsService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Bug>> GetAllBugs()
+        {
+            try
+            {
+                List<Bug> bugs = _service.GetAll();
+                return Ok(bugs);
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Bug> GetOne([FromBody] int id)
+        {
+            try
+            {
+                Bug bug = _service.GetOne(id);
+                return Ok(id);
+            }
+            catch (System.Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- glitchserver/Services/BugsService.cs
+++ glitchserver/Services/BugsService.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using glitchserver.Models;
+
 namespace glitchserver.Services
 {
     public class BugsService
     {
-        
+        internal List<Bug> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Bug GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
