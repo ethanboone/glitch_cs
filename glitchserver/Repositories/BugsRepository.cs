ecd6cbe2c6bfd956fc0cbc3fca3e15de0337ecbf
ethanboone <eboone2019@gmail.com>
2021-06-26 17:03:06 -0600

    added bug service routes

--- glitchserver/Repositories/BugsRepository.cs
+++ glitchserver/Repositories/BugsRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using glitchserver.Models;
+
 namespace glitchserver.Repositories
 {
     public class BugsRepository
     {
-        
+        internal List<Bug> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Bug GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Bug Edit(int id, Bug newBug)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Bug Create(Bug newBug)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
