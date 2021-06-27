becf61c30515f1db85cdca191039f07eac30a15b
ethanboone <eboone2019@gmail.com>
2021-06-27 10:55:36 -0600

    Updated bugs repo

--- glitchserver/Repositories/BugsRepository.cs
+++ glitchserver/Repositories/BugsRepository.cs
@@ -34,22 +34,61 @@ namespace glitchserver.Repositories
 
         internal Bug GetOne(int id)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            SELECT 
+                b.*,
+                a.*
+            FROM bugs b
+            JOIN accounts a ON a.id = b.creatorId
+            WHERE 
+                b.id = @id
+            LIMIT 1;
+            ";
+            return _db.Query<Bug, Account, Bug>(sql, (b, a) =>
+            {
+                b.Creator = a;
+                return b;
+            },
+            new { id }
+            ).FirstOrDefault();
         }
 
         internal bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            DELETE FROM bugs WHERE id = @id LIMIT 1;
+            ";
+            int affectedRows = _db.Execute(sql, new { id });
+            return affectedRows == 1;
         }
 
         internal Bug Edit(int id, Bug newBug)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            UPDATE bugs
+            SET 
+                closed = @Closed,
+                description = @Description,
+                title = @Title,
+                closedDate = @ClosedDate,
+                creatorId = @CreatorId
+            WHERE 
+                id = @id
+            ";
+            _db.Execute(sql, newBug);
+            return newBug;
         }
 
         internal Bug Create(Bug newBug)
         {
-            throw new NotImplementedException();
+            string sql = @"
+            INSERT INTO 
+            bugs (closed, description, title, creatorId)
+            VALUES (@Closed, @Description, @Title, @CreatorId);
+            SELECT LAST_INSERT_ID();
+            ";
+            newBug.Id = _db.ExecuteScalar<int>(sql, newBug);
+            return newBug;
         }
     }
 }
